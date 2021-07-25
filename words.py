8fbb2f9ae9c127a8c4809face773c14d245951d9
Vikash Sharma <87895671+VikashSharma96@users.noreply.github.com>
2021-07-25 17:03:25 +0530

    Create words.py

---
+++ words.py
@@ -0,0 +1,16 @@
+class Add {
+  constructor(...words) {
+      this.words = words;
+  }
+  print = function(){
+    let a = this.words.join("$")
+    console.log("$"+a+"$")
+  }
+}
+
+var x = new Add("hehe", "hoho", "haha", "hihi", "huhu");
+var y = new Add("this", "is", "awesome");
+var z = new Add("lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit");
+x.print();
+y.print();
+z.print();
